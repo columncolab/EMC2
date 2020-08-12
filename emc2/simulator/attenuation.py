0ebc45e7f2de68ffc86b04bbcf63de09d176234f
ISRAEL SILBER <ixs34@bear.met.psu.edu>
2020-08-12 15:09:13 -0400

    Add a ds field for Ze_min profiles

--- emc2/simulator/attenuation.py
+++ emc2/simulator/attenuation.py
@@ -3,6 +3,28 @@ import numpy as np
 from ..core import Instrument
 from ..core.instrument import ureg, quantity
 
+def calc_radar_Ze_min(instrument, model, ref_rng=1000):
+    """
+    This function calculates the minimum detectable radar signal (Ze_min) profile
+    given radar detectability at a reference rnage.
+
+    Parameters
+    ----------
+    instrument: :py:mod:`emc2.core.Instrument`
+        The Instrument class that you wish to calculate Ze_min for.
+    model: :py:mod:`emc2.core.Model`
+        The Model class that you wish to calculate the profile for.
+    ref_rng: scalar
+        Reference altitude for Ze_min
+
+    Returns
+    -------
+    model: :py:mod:`emc2.core.Model`
+        The Model class that will store the Ze_min profile.
+    """
+
+    model.ds["Ze_min"] = instrument.Z_min_1km + 20 * np.log10(model.ds.z) - 20 * np.log10(ref_rng)
+    return model
 
 def calc_radar_atm_attenuation(instrument, model):
     """

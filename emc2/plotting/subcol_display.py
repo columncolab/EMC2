Fix multiple molecular backscatter calculation errors Add COSP emualtor Add phase classification mask plotting options

emc2/plotting/subcol_display.py
@@ -95,7 +95,7 @@ class SubcolumnDisplay(Display):
         self.axes[subplot_index].set_xlim(x_range)
 
     def change_plot_to_class_mask(self, cbar, class_legend=None, variable=None, cbar_label="",
-                                  cmap=None, **kwargs):
+                                  cmap=None, convert_zeros_to_nan=False, **kwargs):
         """
         Updates the colorbar to show phase classification.
 
@@ -127,15 +127,24 @@ class SubcolumnDisplay(Display):
             class_legend = self.model.ds[variable].attrs["legend"]
         l_legend = len(class_legend)
         if cmap is None:
-            cmap = cm.get_cmap("tab20c", lut=l_legend)
-
-        cm.ScalarMappable.set_clim(cbar.mappable, vmin=0.5, vmax=l_legend+0.5)
+            if  convert_zeros_to_nan:
+                cmap = cm.get_cmap("tab20c", lut=l_legend)
+            else:
+                cmap = cm.get_cmap("tab20c", lut=l_legend+1)            
+        if convert_zeros_to_nan:
+            cm.ScalarMappable.set_clim(cbar.mappable, vmin=0.5, vmax=l_legend+0.5)
+        else:
+            cm.ScalarMappable.set_clim(cbar.mappable, vmin=-0.5, vmax=l_legend+0.5)
         cm.ScalarMappable.set_cmap(cbar.mappable, cmap=cmap)
         cbar.set_ticks([x for x in np.arange(1, l_legend+1)])
-        cbar.set_ticklabels(class_legend)
+        if convert_zeros_to_nan:
+            cbar.set_ticks([x for x in np.arange(1, l_legend+1)])
+            cbar.set_ticklabels(class_legend)
+        else:
+            cbar.set_ticks([x for x in np.arange(0, l_legend+1)])
+            cbar.set_ticklabels(["clear"] + class_legend)
         cbar.set_label(cbar_label)
 
-        cbar.set_label(cbar_label)
         return cbar
 
     def plot_subcolumn_timeseries(self, column_no, pressure_coords=True, title=None,

emc2/simulator/attenuation.py
@@ -8,7 +8,6 @@ def calc_radar_Ze_min(instrument, model, ref_rng=1000):
     """
     This function calculates the minimum detectable radar signal (Ze_min) profile
     given radar detectability at a reference rnage.
-
     Parameters
     ----------
     instrument: :py:mod:`emc2.core.Instrument`
@@ -17,7 +16,6 @@ def calc_radar_Ze_min(instrument, model, ref_rng=1000):
         The Model class that you wish to calculate the profile for.
     ref_rng: scalar
         Reference altitude for Ze_min
-
     Returns
     -------
     model: :py:mod:`emc2.core.Model`
@@ -135,12 +133,12 @@ def calc_theory_beta_m(model, Lambda, OD_from_sfc=True):
     n_s = (n_s_ref - 1) * ((1 + alpha * 15) / (1 + alpha * T)) * (P / 1013.25) + 1
     N_s = P * 100 / (R * (T + 273.15)) * Avogadro_c / 1e6
     sigma = 8 * np.pi**3 / 3 * (n_s**2 - 1)**2 / \
-        ((Lambda * 1e-4)**4 * N_s**2) * (6 + 3 * raw_n) / (6 - 7 * raw_n)
+        (N_s * (Lambda * 1e-4)**4 * N_s) * (6 + 3 * raw_n) / (6 - 7 * raw_n)
     beta = 8 * np.pi**3 / 3 * (n_s**2 - 1)**2 * N_s / \
-        ((Lambda * 1e-4)**4 * N_s**2) * (6 + 3 * raw_n) / (6 - 7 * raw_n)
+        (N_s * (Lambda * 1e-4)**4 * N_s) * (6 + 3 * raw_n) / (6 - 7 * raw_n)
     kappa = beta / raw
     sigma_180 = np.pi**2 * (n_s**2 - 1)**2 * 2 * (2 + raw_n) / \
-        ((Lambda * 1e-4)**4 * N_s**2 * (6 - 7 * raw_n)) * p_cos
+        (N_s * (Lambda * 1e-4)**4 * N_s * (6 - 7 * raw_n)) * p_cos
     sigma_180_vol = sigma_180 * N_s
 
     sigma = sigma * 1e-4
@@ -150,13 +148,13 @@ def calc_theory_beta_m(model, Lambda, OD_from_sfc=True):
     sigma_180 = sigma_180 * 1e-4
     sigma_180_vol = sigma_180_vol / 1e-2
 
-    Z_4_trap = np.diff(Z, axis=0) / 2.
-    summed_beta = beta[:-1, :] + beta[1:, :]
+    Z_4_trap = np.diff(Z, axis=1) / 2.
+    summed_beta = beta[:, :-1] + beta[:, 1:]
     u = np.zeros_like(beta)
     if OD_from_sfc:
-        u[1:, :] = np.cumsum(Z_4_trap * summed_beta, axis=0)
+        u[:, 1:] = np.cumsum(Z_4_trap * summed_beta, axis=1)
     else:
-        u[1:, :] = np.flip(np.cumsum(np.flip(Z_4_trap * summed_beta, axis=0), axis=0), axis=0)
+        u[:, :-1] = np.flip(np.cumsum(np.flip(Z_4_trap * summed_beta, axis=1), axis=1), axis=1)
 
     tau = np.exp(-2 * u)
 
@@ -175,7 +173,7 @@ def calc_theory_beta_m(model, Lambda, OD_from_sfc=True):
 
     model.ds["sigma_180_vol"] = xr.DataArray(sigma_180_vol, dims=my_dims)
     model.ds["sigma_180_vol"].attrs["long_name"] = "Volume backscatter cross section"
-    model.ds["sigma_180_vol"].attrs["units"] = "m^2"
+    model.ds["sigma_180_vol"].attrs["units"] = "m^-1"
 
     model.ds["sigma_180"] = xr.DataArray(sigma_180, dims=my_dims)
     model.ds["sigma_180"].attrs["long_name"] = "backscatter cross section per molecule"

emc2/simulator/classification.py
@@ -4,7 +4,8 @@ import numpy as np
 from ..core import Instrument, Model
 
 
-def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None):
+def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None,
+                        convert_zeros_to_nan=False):
     """
     Phase classification based on fixed thresholds of a lidar's LDR and
     tot beta_p variables.
@@ -23,6 +24,9 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None):
         highest beta_p threshold values for a given LDR, that is,
         beta_p[i+1 | LDR=x] >= beta_p[i | LDR=x]. class integer values of 0 = clear.
         When None, the default settings from the instrument will be used.
+    convert_zeros_to_nan: bool
+        If True, saving the mask array as a float dtype (instead of uint8) and converting all
+            zeros in the array to nans.
 
     Returns
     -------
@@ -41,7 +45,10 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None):
     for cloud_class in ["conv", "strat"]:
         mask_name = "%s_phase_mask_%s" % (cloud_class, instrument.instrument_str)
         Class_legend = [""] * (len(beta_p_phase_thresh))
-        phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
+        if convert_zeros_to_nan:
+            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.float)
+        else:
+            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
         for class_type in range(len(beta_p_phase_thresh)):
             phase_mask = np.where(model.ds["sub_col_beta_p_tot_%s" % cloud_class].values >= \
                 np.interp(model.ds["LDR_%s" % cloud_class].values, beta_p_phase_thresh[class_type]["LDR"], 
@@ -49,6 +56,8 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None):
                 beta_p_phase_thresh[class_type]["class_ind"], phase_mask)
             Class_legend[beta_p_phase_thresh[class_type]["class_ind"]-1] = \
                 beta_p_phase_thresh[class_type]["class"]
+        if convert_zeros_to_nan:
+            phase_mask = np.where(phase_mask == 0, np.nan, phase_mask)
 
         model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["LDR_%s" % cloud_class].dims)
         model.ds[mask_name].attrs["long_name"] = "%s phase classification mask" % instrument.instrument_str
@@ -57,7 +66,9 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None):
 
     return model
 
-def lidar_emulate_cosp_phase(instrument, model, phase_disc_curves=None):
+def lidar_emulate_cosp_phase(instrument, model, eta=0.7, OD_from_sfc=True, phase_disc_curve=None,
+                            atb_cross_coeff=None, cloud_SR_thresh=5., undef_SR_thresh=30.,
+                            inc_precip_hyd_atb=True, convert_zeros_to_nan=False):
     """
     Phase classification method to emulate COSP based on attenuated total backscatter
         (ATB) analysis following Cesana and Chepfer (2013).
@@ -68,14 +79,30 @@ def lidar_emulate_cosp_phase(instrument, model, phase_disc_curves=None):
         The instrument to classify. The instrument must be a lidar.
     model: Model
         The model output to classify.
-    beta_p_phase_thresh: list of dicts or None
-        If a list, each index contains a dictionaly with class name, class integer
-        value (mask order), and the phase discrimination polynomial coefficients.
-        In order for the method to operate properly, the list should be arranged
-        from the lowest to highest ATB_cross per ATB_tot, that is,
-        ATB_cross[i+1 | ATB_tot=x] >= ATB_cross[i | ATB_tot=x]. 
-        class integer values of 0 = clear.
-        When None, the default settings from the instrument will be used.
+    eta: float
+        Multiple scattering coefficient.
+    OD_from_sfc: bool
+        If True, optical depth will be calculated from the surface. If False,
+        optical depth will be calculated from the top of the atmosphere.
+    phase_disc_curves: list or None
+        Phase discrimination curve polynomial coefficients (above - liquid, below - ice).
+        When None, the default settings from this method will be used (following Cesana
+        and Chepfer (2013).
+    atb_cross_coeff: dict or None.
+        Dictionary of polynomial coefficients for the estimation of ATB_cross for each
+        hydrometeor type (dict keys).
+        When None, the default coefficients from Cesana and Chepfer (2013) / COSP are used.
+    cloud_SR_thresh: float
+        Scattering ratio threshold for hydrometeor detection.
+    undef_SR_thresh: float
+        Scattering ratio threshold for the undefined phase (layers below a layer with
+        SR values above this threshold are all set to set to "undefined").
+    inc_precip_hyd_atb: bool
+        If True, include precipitating classes in the calculation of ATB (not specified in
+            the referencedpaper, so likely false in actual COSP).
+    convert_zeros_to_nan: bool
+        If True, saving the mask array as a float dtype (instead of uint8) and converting all
+            zeros in the array to nans.
 
     Returns
     -------
@@ -83,30 +110,102 @@ def lidar_emulate_cosp_phase(instrument, model, phase_disc_curves=None):
         The model with the added simulated lidar parameters.
     """
 
-    if not instrument.instrument_class.lower() == "lidar":
-                raise ValueError("Instrument must be a lidar!")
+    if instrument.instrument_str is not "HSRL":
+                raise Valuerror("Instrument must be the 532 nm HSRL (to match CALIOP's operating wavelength)")
 
-    if beta_p_phase_thresh is None:
-        if not instrument.beta_p_phase_thresh:
-                raise ValueError("no default threshold values for %s" % instrument.instrument_str)
-        beta_p_phase_thresh = instrument.beta_p_phase_thresh
+    if phase_disc_curve is None:
+        phase_disc_curve = [9.032e3, -2.136e3, 173.394, -3.951, 0.256, -9.478e-4]
+
+    if atb_cross_coeff is None:
+        atb_cross_coeff = {'liq': [0.4099, 0.009, 0], 'ice': [0.2904, 0]}
+
+    if inc_precip_hyd_atb:
+        hyd_groups = {"liq": ["cl", "pl"], "ice": ["ci", "pi"]}
+    else:
+        hyd_groups = {"liq": ["cl"], "ice": ["ci"]}
+
+    ATB_mol = np.tile(model.ds['sigma_180_vol'].values * (1.+0.0284) * model.ds['tau'].values, \
+               (model.num_subcolumns, 1, 1))
 
     for cloud_class in ["conv", "strat"]:
-        mask_name = "%s_phase_mask_%s" % (cloud_class, instrument.instrument_str)
-        Class_legend = [""] * (len(beta_p_phase_thresh))
+        mask_name = "%s_COSP_phase_mask" % cloud_class
+        Class_legend = ["liquid", "ice", "undefined"]
         phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
-        for class_type in range(len(beta_p_phase_thresh)):
-            phase_mask = np.where(model.ds["sub_col_beta_p_tot_%s" % cloud_class].values >= \
-                np.interp(model.ds["LDR_%s" % cloud_class].values, beta_p_phase_thresh[class_type]["LDR"],
-                beta_p_phase_thresh[class_type]["beta_p"]),
-                beta_p_phase_thresh[class_type]["class_ind"], phase_mask)
-            Class_legend[beta_p_phase_thresh[class_type]["class_ind"]-1] = \
-                beta_p_phase_thresh[class_type]["class"]
+        ATB_co = {}
+        ATB_cross = {}
+        OD = {}
+        beta_p = {}
+        beta_p_cross = {}
+        ATB = {}
+        for hyd_class in hyd_groups.keys():
+            OD[hyd_class] = np.zeros_like(model.ds['sub_col_beta_p_tot_strat'].values)
+            beta_p[hyd_class] = np.zeros_like(model.ds['sub_col_beta_p_tot_strat'].values)
+            for class_name in hyd_groups[hyd_class]:
+                beta_p[hyd_class] += model.ds['sub_col_beta_p_%s_%s' % (class_name, cloud_class)].values
+                OD[hyd_class] += model.ds['sub_col_OD_%s_%s' % (class_name, cloud_class)].values
+            ATB_co[hyd_class] =  (np.tile(model.ds['sigma_180_vol'].values, (model.num_subcolumns, 1, 1)) + \
+                    beta_p[hyd_class]) * np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1)) * \
+                    np.exp(-2 * eta * OD[hyd_class])
+            ATB_cross[hyd_class] = np.polyval(atb_cross_coeff[hyd_class], ATB_co[hyd_class] * 1e3) / 1e3
+            beta_p_cross[hyd_class] = ATB_cross[hyd_class] / np.exp(-2 * eta * OD[hyd_class]) / \
+                    np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1)) - \
+                    np.tile(model.ds['sigma_180_vol'].values * (0.0284/(1+0.0284)), (model.num_subcolumns, 1, 1))
+            ATB[hyd_class] = (beta_p[hyd_class] + beta_p_cross[hyd_class] + \
+                    np.tile(model.ds['sigma_180_vol'].values * (1.+0.0284), (model.num_subcolumns, 1, 1))) * \
+                    np.exp(-2 * eta * OD[hyd_class]) * \
+                    np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1))
+        ATB_tot = (beta_p["liq"] + beta_p_cross["liq"] + beta_p["ice"] + beta_p_cross["ice"] + \
+                    np.tile(model.ds['sigma_180_vol'].values * (1.+0.0284), (model.num_subcolumns, 1, 1))) * \
+                    np.exp(-2 * eta * (OD["liq"] + OD["ice"])) * \
+                    np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1))
+        ATB_cross_tot = (beta_p_cross["liq"] + beta_p_cross["ice"] + np.tile(model.ds['sigma_180_vol'].values * \
+                    (0.0284/(1.+0.0284)), (model.num_subcolumns, 1, 1))) * \
+                    np.exp(-2 * eta * (OD["liq"] + OD["ice"])) * \
+                    np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1))
+        del ATB, beta_p_cross, ATB_cross, ATB_co, OD, beta_p
+
+        # Begin cloud detection and phase classification
+        SR = ATB_tot / ATB_mol
+        if convert_zeros_to_nan:
+            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.float)
+        else:
+            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
+        phase_mask = np.where(SR > cloud_SR_thresh, 1, phase_mask)
+        phase_mask = np.where(np.logical_and(ATB_cross_tot > np.polyval(phase_disc_curve, ATB_tot * 1e3) / 1e3,
+                phase_mask == 1), 2, phase_mask)
+        if OD_from_sfc:
+            reflective_mask = np.cumsum(SR > undef_SR_thresh, axis=2)
+        else:
+            reflective_mask = np.flip(np.cumsum(np.flip(SR, axis=2) > undef_SR_thresh, axis=2), axis=2)
+        reflective_mask = np.where(np.logical_and(SR > undef_SR_thresh, reflective_mask == 1),
+            0, reflective_mask)
+
+        phase_mask = np.where(np.logical_and(np.tile(model.ds['t'].values, (model.num_subcolumns, 1, 1)) > 273.15,
+                        phase_mask > 0), 1, phase_mask)
+        phase_mask = np.where(np.logical_and(np.tile(model.ds['t'].values, (model.num_subcolumns, 1, 1)) < 233.15,
+                        phase_mask > 0), 2, phase_mask)
+        phase_mask = np.where(np.logical_and(reflective_mask > 0, phase_mask > 0), 3, phase_mask)
+        if convert_zeros_to_nan:
+            phase_mask = np.where(phase_mask == 0, np.nan, phase_mask)
 
         model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["LDR_%s" % cloud_class].dims)
-        model.ds[mask_name].attrs["long_name"] = "%s phase classification mask" % instrument.instrument_str
+        model.ds[mask_name].attrs["long_name"] = "COSP emulation phase classification mask"
         model.ds[mask_name].attrs["units"] = "Unitless"
-        model.ds[mask_name].attrs["legend"] = Class_legend
+        model.ds[mask_name].attrs["legend"] = ["liquid", "ice", "undef"]
+
+        # save ATB and SR fields for stratiform only
+        if cloud_class is "strat":
+            model.ds["COSP_ATBtot_strat"] = xr.DataArray(ATB_tot, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_ATBtot_strat"].attrs["long_name"] = "COSP emulation ATB_tot"
+            model.ds["COSP_ATBtot_strat"].attrs["units"] = "m^-1sr^-1"
+
+            model.ds["COSP_ATBcross_strat"] = xr.DataArray(ATB_cross_tot, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_ATBcross_strat"].attrs["long_name"] = "COSP emulation ATB_cross"
+            model.ds["COSP_ATBcross_strat"].attrs["units"] = "m^-1sr^-1"
+
+            model.ds["COSP_SR_strat"] = xr.DataArray(SR, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_SR_strat"].attrs["long_name"] = "COSP emulation scattering ratio"
+            model.ds["COSP_SR_strat"].attrs["units"] = "m^-1sr^-1"
 
     return model
 

emc2/simulator/lidar_moments.py
@@ -68,7 +68,7 @@ def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None):
 
 
 def calc_lidar_moments(instrument, model, is_conv,
-                       OD_from_sfc=True, parallel=True, eta=None, **kwargs):
+                       OD_from_sfc=True, parallel=True, eta=1, **kwargs):
     """
     Calculates the lidar backscatter, extinction, and optical depth
     in a given column for the given lidar.
@@ -88,8 +88,7 @@ def calc_lidar_moments(instrument, model, is_conv,
     parallel: bool
         If True, use parallelism in calculating lidar parameters.
     eta: float
-        Multiple scattering coefficient. If None, using a default
-        value of 1.
+        Multiple scattering coefficient.
     Additonal keyword arguments are passed into
     :py:func:`emc2.simulator.lidar_moments.calc_LDR_and_ext`.
 
@@ -110,9 +109,6 @@ def calc_lidar_moments(instrument, model, is_conv,
     t_field = model.T_field
     z_field = model.z_field
     column_ds = model.ds
-
-    if eta is None:
-        eta = 1
 
     # Do unit conversions using pint - pressure in Pa, T in K, z in m
     p_temp = model.ds[p_field].values * getattr(ureg, model.ds[p_field].attrs["units"])
@@ -195,7 +191,7 @@ def calc_lidar_moments(instrument, model, is_conv,
             "Optical depth from all hydrometeors in convective clouds"
         model.ds["sub_col_OD_tot_conv"].attrs["units"] = "1"
 
-        model.ds['sub_col_beta_att_tot_conv'] = beta_m + model.ds['sub_col_beta_p_tot_conv'] * \
+        model.ds['sub_col_beta_att_tot_conv'] = (beta_m + model.ds['sub_col_beta_p_tot_conv']) * \
             T * np.exp(-2 * eta * model.ds['sub_col_OD_tot_conv'])
         model.ds["sub_col_beta_att_tot_conv"].attrs["long_name"] = \
             "Backscatter coefficient from all hydrometeors in convective clouds including gaseous attenuation"
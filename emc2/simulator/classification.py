Add total (conv + strat) radar and lidar variables; fix calculation issues; update metadata

emc2/simulator/classification.py
@@ -46,12 +46,13 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None,
         mask_name = "%s_phase_mask_%s" % (cloud_class, instrument.instrument_str)
         Class_legend = [""] * (len(beta_p_phase_thresh))
         if convert_zeros_to_nan:
-            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.float)
+            phase_mask = np.zeros_like(model.ds["sub_col_LDR_%s" % cloud_class], dtype=np.float)
         else:
-            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
+            phase_mask = np.zeros_like(model.ds["sub_col_LDR_%s" % cloud_class], dtype=np.uint8)
         for class_type in range(len(beta_p_phase_thresh)):
             phase_mask = np.where(model.ds["sub_col_beta_p_tot_%s" % cloud_class].values >= \
-                np.interp(model.ds["LDR_%s" % cloud_class].values, beta_p_phase_thresh[class_type]["LDR"], 
+                np.interp(model.ds["sub_col_LDR_%s" % cloud_class].values,
+                beta_p_phase_thresh[class_type]["LDR"], 
                 beta_p_phase_thresh[class_type]["beta_p"]),
                 beta_p_phase_thresh[class_type]["class_ind"], phase_mask)
             Class_legend[beta_p_phase_thresh[class_type]["class_ind"]-1] = \
@@ -59,7 +60,7 @@ def lidar_classify_phase(instrument, model, beta_p_phase_thresh=None,
         if convert_zeros_to_nan:
             phase_mask = np.where(phase_mask == 0, np.nan, phase_mask)
 
-        model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["LDR_%s" % cloud_class].dims)
+        model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["sub_col_LDR_%s" % cloud_class].dims)
         model.ds[mask_name].attrs["long_name"] = "%s phase classification mask" % instrument.instrument_str
         model.ds[mask_name].attrs["units"] = "Unitless"
         model.ds[mask_name].attrs["legend"] = Class_legend
@@ -189,7 +190,7 @@ def lidar_emulate_cosp_phase(instrument, model, eta=0.7, OD_from_sfc=True, phase
     for cloud_class in ["conv", "strat"]:
         mask_name = "%s_COSP_phase_mask" % cloud_class
         Class_legend = ["liquid", "ice", "undefined"]
-        phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
+        phase_mask = np.zeros_like(model.ds["sub_col_LDR_%s" % cloud_class], dtype=np.uint8)
         ATB_co = {}
         ATB_cross = {}
         OD = {}
@@ -226,9 +227,9 @@ def lidar_emulate_cosp_phase(instrument, model, eta=0.7, OD_from_sfc=True, phase
         # Begin cloud detection and phase classification
         SR = ATB_tot / ATB_mol
         if convert_zeros_to_nan:
-            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.float)
+            phase_mask = np.zeros_like(model.ds["sub_col_LDR_%s" % cloud_class], dtype=np.float)
         else:
-            phase_mask = np.zeros_like(model.ds["LDR_%s" % cloud_class], dtype=np.uint8)
+            phase_mask = np.zeros_like(model.ds["sub_col_LDR_%s" % cloud_class], dtype=np.uint8)
         phase_mask = np.where(SR > cloud_SR_thresh, 1, phase_mask)
         phase_mask = np.where(np.logical_and(ATB_cross_tot > np.polyval(phase_disc_curve, ATB_tot * 1e3) / 1e3,
                 phase_mask == 1), 2, phase_mask)
@@ -247,22 +248,23 @@ def lidar_emulate_cosp_phase(instrument, model, eta=0.7, OD_from_sfc=True, phase
         if convert_zeros_to_nan:
             phase_mask = np.where(phase_mask == 0, np.nan, phase_mask)
 
-        model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["LDR_%s" % cloud_class].dims)
+        model.ds[mask_name] = xr.DataArray(phase_mask, dims=model.ds["sub_col_LDR_%s" % cloud_class].dims)
         model.ds[mask_name].attrs["long_name"] = "COSP emulation phase classification mask"
         model.ds[mask_name].attrs["units"] = "Unitless"
         model.ds[mask_name].attrs["legend"] = ["liquid", "ice", "undef"]
 
         # save ATB and SR fields for stratiform only
         if cloud_class is "strat":
-            model.ds["COSP_ATBtot_strat"] = xr.DataArray(ATB_tot, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_ATBtot_strat"] = xr.DataArray(ATB_tot, dims=model.ds["sub_col_LDR_%s" % cloud_class].dims)
             model.ds["COSP_ATBtot_strat"].attrs["long_name"] = "COSP emulation ATB_tot"
             model.ds["COSP_ATBtot_strat"].attrs["units"] = "m^-1sr^-1"
 
-            model.ds["COSP_ATBcross_strat"] = xr.DataArray(ATB_cross_tot, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_ATBcross_strat"] = xr.DataArray(ATB_cross_tot,
+                        dims=model.ds["sub_col_LDR_%s" % cloud_class].dims)
             model.ds["COSP_ATBcross_strat"].attrs["long_name"] = "COSP emulation ATB_cross"
             model.ds["COSP_ATBcross_strat"].attrs["units"] = "m^-1sr^-1"
 
-            model.ds["COSP_SR_strat"] = xr.DataArray(SR, dims=model.ds["LDR_%s" % cloud_class].dims)
+            model.ds["COSP_SR_strat"] = xr.DataArray(SR, dims=model.ds["sub_col_LDR_%s" % cloud_class].dims)
             model.ds["COSP_SR_strat"].attrs["long_name"] = "COSP emulation scattering ratio"
             model.ds["COSP_SR_strat"].attrs["units"] = "m^-1sr^-1"
 

emc2/simulator/lidar_moments.py
@@ -8,10 +8,9 @@ from .psd import calc_mu_lambda
 from ..core.instrument import ureg, quantity
 
 
-def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None, chunk=None):
+def calc_total_alpha_beta(model, OD_from_sfc=True, eta=1):
     """
-    Calculates the lidar extinction mask (for conv+strat) and linear depolarization ratio
-    (per strat or conv) for the given model and lidar.
+    Calculates total (strat+conv) lidar variables.
 
     Parameters
     ----------
@@ -26,10 +25,57 @@ def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None, chunk
         If a dict, the amount of LDR per hydrometeor class must be specified in
         a dictionary whose keywords are the model's hydrometeor classes. If None,
         the default settings from the model will be used.
-    chunk: None or int
-        If using parallel processing, only send this number of time periods to the
-        parallel loop at one time. Sometimes Dask will crash if there are too many
-        tasks in the queue, so setting this value will help avoid that.
+    eta: float
+        Multiple scattering coefficient.
+
+    Returns
+    -------
+    model: Model
+        The model with the added simulated lidar parameters.
+    """
+
+
+    model.ds["sub_col_beta_p_tot"] = model.ds["sub_col_beta_p_tot_conv"] + model.ds["sub_col_beta_p_tot_strat"]
+    model.ds["sub_col_alpha_p_tot"] = model.ds["sub_col_alpha_p_tot_conv"] + model.ds["sub_col_alpha_p_tot_strat"]
+    model.ds["sub_col_OD_tot"] = model.ds["sub_col_OD_tot_conv"] + model.ds["sub_col_OD_tot_strat"]
+    model.ds["sub_col_beta_p_tot"].attrs["long_name"] = \
+        "Total backscatter coefficient (convective + stratiform)"
+    model.ds["sub_col_beta_p_tot"].attrs["units"] = "m^-1"
+    model.ds["sub_col_alpha_p_tot"].attrs["long_name"] = \
+        "Total extinction coefficient (convective + stratiform)"
+    model.ds["sub_col_alpha_p_tot"].attrs["units"] = "m^-1"
+    model.ds["sub_col_OD_tot"].attrs["long_name"] = \
+        "Total optical depth (convective + stratiform)"
+    model.ds["sub_col_OD_tot"].attrs["units"] = "1"
+    beta_m = np.tile(model.ds['sigma_180_vol'].values, (model.num_subcolumns, 1, 1))
+    T = np.tile(model.ds['tau'].values, (model.num_subcolumns, 1, 1))
+    model.ds['sub_col_beta_att_tot'] = (beta_m + model.ds['sub_col_beta_p_tot']) * \
+        T * np.exp(-2 * eta * model.ds['sub_col_OD_tot'])
+    model.ds["sub_col_beta_att_tot"].attrs["long_name"] = \
+        "Total attenuated backscatter coefficient (convective + stratiform)"
+    model.ds["sub_col_beta_att_tot"].attrs["units"] = "m^-1"
+
+    return model
+
+
+def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None):
+    """
+    Calculates the lidar extinction mask (for conv+strat) and linear depolarization ratio
+    (per strat, conv, and strat+conv) for the given model and lidar.
+
+    Parameters
+    ----------
+    model: Model
+        The model to generate the parameters for.
+    ext_OD: float
+        The optical depth threshold for determining if the signal is extinct.
+    OD_from_sfc: bool
+        If True, optical depth will be calculated from the surface. If False,
+        optical depth will be calculated from the top of the atmosphere.
+    LDR_per_hyd: dict or None
+        If a dict, the amount of LDR per hydrometeor class must be specified in
+        a dictionary whose keywords are the model's hydrometeor classes. If None,
+        the default settings from the model will be used.
 
     Returns
     -------
@@ -40,6 +86,8 @@ def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None, chunk
     if LDR_per_hyd is None:
         LDR_per_hyd = model.LDR_per_hyd
 
+    numerator_tot = xr.zeros_like(model.ds["sub_col_beta_p_%s_strat" % model.hydrometeor_classes[0]])
+    denominator_tot = xr.zeros_like(model.ds["sub_col_beta_p_%s_strat" % model.hydrometeor_classes[0]])
     for cloud_class in ["conv", "strat"]:
         numerator = 0.
         denominator = 0.
@@ -49,9 +97,15 @@ def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None, chunk
             numerator += model.ds[beta_p_key] * model.LDR_per_hyd[hyd_type].magnitude
             denominator += model.ds[beta_p_key]
 
-        model.ds["LDR_%s" % cloud_class] = numerator / denominator
-        model.ds["LDR_%s" % cloud_class].attrs["long_name"] = "Linear depolarization ratio in %s" % cloud_class
-        model.ds["LDR_%s" % cloud_class].attrs["units"] = "1"
+        model.ds["sub_col_LDR_%s" % cloud_class] = numerator / denominator
+        model.ds["sub_col_LDR_%s" % cloud_class].attrs["long_name"] = "Linear depolarization ratio in %s" % cloud_class
+        model.ds["sub_col_LDR_%s" % cloud_class].attrs["units"] = "1"
+        numerator_tot += numerator
+        denominator += denominator
+    model.ds["sub_col_LDR_tot"] = numerator_tot / denominator_tot
+    model.ds["sub_col_LDR_tot"].attrs["long_name"] = "Linear depolarization ratio (convective + stratiform)"
+    model.ds["sub_col_LDR_tot"].attrs["units"] = "1"
+
 
     OD_cum_p_tot = model.ds["sub_col_OD_tot_strat"].values + model.ds["sub_col_OD_tot_conv"].values
     OD_cum_p_tot = np.where(OD_cum_p_tot > ext_OD, 2, 0.)
@@ -62,8 +116,8 @@ def calc_LDR_and_ext(model, ext_OD=4., OD_from_sfc=True, LDR_per_hyd=None, chunk
     ext_tmp = np.where(my_diff > 1., 1, 0)
     ext_mask = OD_cum_p_tot - ext_tmp
 
-    model.ds["ext_mask"] = xr.DataArray(ext_mask, dims=model.ds["LDR_conv"].dims)
-    model.ds["ext_mask"].attrs["long_name"] = "Extinction mask"
+    model.ds["ext_mask"] = xr.DataArray(ext_mask, dims=model.ds["sub_col_LDR_conv"].dims)
+    model.ds["ext_mask"].attrs["long_name"] = "Extinction mask (convective + stratiform)"
     model.ds["ext_mask"].attrs["units"] = ("2 = Signal extinct, 1 = layer where signal becomes " +
                                            "extinct, 0 = signal not extinct")
 
@@ -202,7 +256,7 @@ def calc_lidar_moments(instrument, model, is_conv,
         model.ds['sub_col_beta_att_tot_conv'] = (beta_m + model.ds['sub_col_beta_p_tot_conv']) * \
             T * np.exp(-2 * eta * model.ds['sub_col_OD_tot_conv'])
         model.ds["sub_col_beta_att_tot_conv"].attrs["long_name"] = \
-            "Backscatter coefficient from all hydrometeors in convective clouds including gaseous attenuation"
+            "Backscatter coefficient from all hydrometeors in convective clouds"
         model.ds["sub_col_beta_att_tot_conv"].attrs["units"] = "m^-1"
         return model
     else:
@@ -304,7 +358,7 @@ def calc_lidar_moments(instrument, model, is_conv,
         model.ds['sub_col_beta_att_tot_strat'] = (beta_m + model.ds['sub_col_beta_p_tot_strat']) * \
             T * np.exp(-2 * eta * model.ds['sub_col_OD_tot_strat'])
         model.ds["sub_col_beta_att_tot_strat"].attrs["long_name"] = \
-            "Attenuated total backscatter in stratiform clouds including gaseous attenuation"
+            "Attenuated total backscatter in stratiform clouds"
         model.ds["sub_col_beta_att_tot_strat"].attrs["units"] = "m^-1"
 
         return model

emc2/simulator/radar_moments.py
@@ -9,6 +9,38 @@ from .psd import calc_mu_lambda
 from ..core.instrument import ureg
 
 
+def calc_total_reflectivity(model):
+    """
+    This method calculates the total (convective + stratiform) reflectivity (Ze).
+
+    Parameters
+    ----------
+    model: :func:`emc2.core.Model` class
+        The model to calculate the parameters for.
+
+
+    Returns
+    -------
+    model: :func:`emc2.core.Model`
+        The xarray Dataset containing the calculated radar moments.
+    """
+    Ze_tot = np.where(np.isfinite(model.ds["sub_col_Ze_tot_strat"].values),
+                10**(model.ds["sub_col_Ze_tot_strat"].values / 10.), 0)
+    Ze_tot = np.where(np.isfinite(model.ds["sub_col_Ze_tot_conv"].values), Ze_tot +
+                10**(model.ds["sub_col_Ze_tot_conv"].values / 10.), np.where(Ze_tot > 0, Ze_tot, np.nan))
+
+    model.ds['sub_col_Ze_tot'] = xr.DataArray(10 * np.log10(Ze_tot), dims=model.ds["sub_col_Ze_tot_strat"].dims)
+    model.ds['sub_col_Ze_tot'].attrs["long_name"] = \
+                "Total (convective + stratiform) equivalent radar reflectivity factor"
+    model.ds['sub_col_Ze_tot'].attrs["units"] = "dBZ"
+    model.ds['sub_col_Ze_att_tot'] = 10 * np.log10(Ze_tot * \
+                model.ds['hyd_ext_conv'] * model.ds['hyd_ext_strat'] * model.ds['atm_ext'])
+    model.ds['sub_col_Ze_att_tot'].attrs["long_name"] = \
+                "Total (convective + stratiform) attenuated (hydrometeor + gaseous) equivalent radar reflectivity factor"
+    model.ds['sub_col_Ze_att_tot'].attrs["units"] = "dBZ"
+    return model
+
+
 def calc_radar_reflectivity_conv(instrument, model, hyd_type):
     """
     This estimates the radar reflectivity using empirical Ze-q_hyd relationships, where
@@ -126,30 +158,35 @@ def calc_radar_moments(instrument, model, is_conv,
 
         kappa_ds = calc_radar_atm_attenuation(instrument, model)
         kappa_f = 6 * np.pi / instrument.wavelength * 1e-6 * model.Rho_hyd["cl"].magnitude
-        WC = column_ds[q_names["cl"]] + column_ds[q_names["pl"]] * 1e3 * \
+        WC = column_ds["conv_q_subcolumns_cl"] + column_ds["conv_q_subcolumns_pl"] * 1e3 * \
             column_ds[p_field] / (instrument.R_d * column_ds[t_field])
 
         WC_new = np.zeros_like(WC)
         if OD_from_sfc:
-            dz = np.diff(column_ds[z_field].values, axis=1, prepend=0.)
-            WC_new[:, 1:] = WC[:, :-1]
-            liq_ext = np.cumsum(kappa_f * dz * WC_new, axis=1)
+            dz = np.diff(column_ds[z_field].values / 1e3, axis=1, prepend=0.)
+            WC_new[:, :, 1:] = WC[:, :, :-1]
+            liq_ext = np.cumsum(np.tile(kappa_f * dz, (model.num_subcolumns, 1, 1)) * WC_new, axis=2)
             atm_ext = np.cumsum(kappa_ds.ds["kappa_att"].values * dz, axis=1)
         else:
-            dz = np.diff(column_ds[z_field].values, axis=1, append=0.)
-            WC_new[:, :-1] = WC[:, 1:]
-            liq_ext = np.flip(np.cumsum(np.flip(kappa_f * dz * WC_new, axis=1), axis=1), axis=1)
+            dz = np.diff(column_ds[z_field].values / 1e3, axis=1, append=0.)
+            WC_new[:, :, :-1] = WC[:, :, 1:]
+            liq_ext = np.flip(np.cumsum(np.flip(np.tile(kappa_f * dz, (model.num_subcolumns, 1, 1)) * \
+                            WC_new, axis=2), axis=2), axis=2)
             atm_ext = np.flip(np.cumsum(np.flip(kappa_ds.ds["kappa_att"].values * dz, axis=1), axis=1), axis=1)
 
         if len(liq_ext.shape) == 1:
             liq_ext = liq_ext[:, np.newaxis]
         if len(atm_ext.shape) == 1:
             atm_ext = atm_ext[:, np.newaxis]
-        liq_ext = xr.DataArray(liq_ext, dims=kappa_ds.ds["kappa_att"].dims)
-        atm_ext = xr.DataArray(atm_ext, dims=kappa_ds.ds["kappa_att"].dims)
 
-        column_ds["sub_col_Ze_att_tot_conv"] = column_ds["sub_col_Ze_tot_conv"] / \
-            10**(2 * liq_ext / 10.) / 10**(2 * atm_ext / 10.)
+        column_ds['hyd_ext_conv'] = xr.DataArray(10**(-2 * liq_ext / 10.), dims=WC.dims)
+        column_ds['hyd_ext_conv'].attrs["long_name"] = "Two-way convective hydrometeor transmittance"
+        column_ds['hyd_ext_conv'].attrs["units"] = "1"
+        column_ds['atm_ext'] = xr.DataArray(10**(-2 * atm_ext / 10), dims=kappa_ds.ds["kappa_att"].dims)
+        column_ds['atm_ext'].attrs["long_name"] = "Two-way atmospheric transmittance due to H2O and O2"
+        column_ds['atm_ext'].attrs["units"] = "1"
+        column_ds["sub_col_Ze_att_tot_conv"] = column_ds["sub_col_Ze_tot_conv"] * \
+            column_ds['hyd_ext_conv'] * column_ds['atm_ext']
         column_ds["sub_col_Ze_tot_conv"] = column_ds["sub_col_Ze_tot_conv"].where(
             column_ds["sub_col_Ze_tot_conv"] != 0)
         column_ds["sub_col_Ze_att_tot_conv"].attrs["long_name"] = \
@@ -336,17 +373,23 @@ def calc_radar_moments(instrument, model, is_conv,
 
     if OD_from_sfc:
         dz = np.diff(column_ds[z_field].values, axis=1, prepend=0.)
-        dz = np.tile(dz, (model.num_subcolumns, 1, 1))
-        od_tot = np.cumsum(dz * od_tot, axis=2)
-        atm_ext = np.cumsum(dz / 1e3 * kappa_ds.ds['kappa_att'].values, axis=2)
+        od_tot = np.cumsum(np.tile(dz, (model.num_subcolumns, 1, 1)) * od_tot, axis=2)
+        atm_ext = np.cumsum(dz / 1e3 * kappa_ds.ds['kappa_att'].values, axis=1)
     else:
-        dz = np.diff(column_ds[z_field].values, append=0.)
-        dz = np.tile(dz, (model.num_subcolumns, 1, 1))
-        od_tot = np.flip(np.cumsum(np.flip(dz * od_tot, axis=2), axis=2), axis=2)
-        atm_ext = np.flip(np.cumsum(np.flip(dz / 1e3 * kappa_ds.ds['kappa_att'].values, axis=2), axis=2), axis=2)
+        dz = np.diff(column_ds[z_field].values, axis=1, append=0.)
+        od_tot = np.flip(np.cumsum(np.flip(np.tile(dz, (model.num_subcolumns, 1, 1)) * \
+                        od_tot, axis=2), axis=2), axis=2)
+        atm_ext = np.flip(np.cumsum(np.flip(dz / 1e3 * kappa_ds.ds['kappa_att'].values, axis=1), axis=1), axis=1)
+
+    column_ds['hyd_ext_strat'] = xr.DataArray(np.exp(-2 * od_tot), dims=kappa_ds.ds["sub_col_Ze_tot_strat"].dims)
+    column_ds['hyd_ext_strat'].attrs["long_name"] = "Two-way stratiform hydrometeor transmittance"
+    column_ds['hyd_ext_strat'].attrs["units"] = "1"
+    column_ds['atm_ext'] = xr.DataArray(10**(-2 * atm_ext / 10), dims=kappa_ds.ds["kappa_att"].dims)
+    column_ds['atm_ext'].attrs["long_name"] = "Two-way atmospheric transmittance due to H2O and O2"
+    column_ds['atm_ext'].attrs["units"] = "1"
 
     column_ds['sub_col_Ze_att_tot_strat'] = \
-        column_ds['sub_col_Ze_tot_strat'] * np.exp(-2 * od_tot) / 10**(2 * atm_ext / 10)
+        column_ds['sub_col_Ze_tot_strat'] * column_ds['hyd_ext_strat'] * column_ds['atm_ext']
     column_ds['sub_col_Ze_tot_strat'] = column_ds['sub_col_Ze_tot_strat'].where(
         column_ds['sub_col_Ze_tot_strat'] > 0)
     column_ds['sub_col_Ze_att_tot_strat'] = column_ds['sub_col_Ze_att_tot_strat'].where(
@@ -354,7 +397,7 @@ def calc_radar_moments(instrument, model, is_conv,
     column_ds['sub_col_Ze_tot_strat'] = 10 * np.log10(column_ds['sub_col_Ze_tot_strat'])
     column_ds['sub_col_Ze_att_tot_strat'] = 10 * np.log10(column_ds['sub_col_Ze_att_tot_strat'])
     column_ds['sub_col_Ze_att_tot_strat'].attrs["long_name"] = \
-        "Radar reflectivity factor in stratiform clouds factoring in gaseous attenuation"
+        "Radar reflectivity factor in stratiform clouds factoring in gaseous and hydrometeor attenuation"
     column_ds['sub_col_Ze_att_tot_strat'].attrs["units"] = "dBZ"
     column_ds['sub_col_Ze_tot_strat'].attrs["long_name"] = \
         "Radar reflectivity factor in stratiform clouds"
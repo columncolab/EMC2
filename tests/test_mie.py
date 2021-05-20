8c3f364c992dbe0980bf151e2440c41b65f54352
Israel Silber <ixs34@psu.edu>
2021-05-20 15:29:12 -0400

    Update test_io.py

--- tests/test_mie.py
+++ tests/test_mie.py
@@ -6,7 +6,7 @@ def test_mie_liquid():
     KAZR = emc2.core.instruments.KAZR('nsa')
     KAZR_mie = emc2.scattering.scat_properties_water(
         KAZR.mie_table["cl"].p_diam * 1e6, KAZR.wavelength * 1e-4, 0.)
-    assert np.corrcoef(KAZR_mie.alpha_p.values, KAZR.mie_table["cl"].alpha_p.values)[0, 1] > 0.8
+    assert np.corrcoef(KAZR_mie.alpha_p.values, KAZR.mie_table["cl"].alpha_p.values)[0, 1] > 0.9
     assert np.corrcoef(KAZR_mie.beta_p.values, KAZR.mie_table["cl"].beta_p.values)[0, 1] > 0.8
     assert np.corrcoef(KAZR_mie.scat_p.values, KAZR.mie_table["cl"].scat_p.values)[0, 1] > 0.8
     np.testing.assert_allclose(KAZR_mie.p_diam.values, KAZR.mie_table["cl"].p_diam.values, rtol=1e-2)

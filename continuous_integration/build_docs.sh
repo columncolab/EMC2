333592ebbd3cdf37b6f1c22a79ec6d8ce565f01b
Israel Silber <ixs34@psu.edu>
2021-05-22 20:54:03 -0400

    Try the nbsphinx version that worked in the last working pull (version was updated on May 12).

--- continuous_integration/build_docs.sh
+++ continuous_integration/build_docs.sh
@@ -5,7 +5,7 @@ conda install -c conda-forge -q sphinx doctr pandoc
 conda install numpydoc 
 pip install sphinx_gallery
 pip install sphinx-copybutton
-conda install -c conda-forge nbsphinx
+conda install -c conda-forge nbsphinx=0.8.3=pyhd8ed1ab_0
 cd doc
 make clean
 make html

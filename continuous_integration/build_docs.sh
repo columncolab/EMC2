d247c2dd56d905d100bf31f1d4367e5a6c566638
Israel Silber <ixs34@psu.edu>
2021-06-03 14:52:20 -0400

    Restore build_docs.sh

--- continuous_integration/build_docs.sh
+++ continuous_integration/build_docs.sh
@@ -5,7 +5,7 @@ conda install -c conda-forge -q sphinx doctr pandoc
 conda install numpydoc 
 pip install sphinx_gallery
 pip install sphinx-copybutton
-conda install -c conda-forge nbsphinx=0.8.3=pyhd8ed1ab_0
+conda install -c conda-forge nbsphinx
 cd doc
 make clean
 make html

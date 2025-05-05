ae6b82ea3c0e7f60a8911012529760b4f0bd0b9e
Israel Silber <55999320+isilber@users.noreply.github.com>
2025-05-05 16:57:47 -0700

    FIX: inconsistent mu and lambda calculation when N_sub=1 (subcolumn g… (#138)
    * FIX: inconsistent mu and lambda calculation when N_sub=1 (subcolumn generator turned off).
    MNT: some limited code refactoring.
    
    * TST: update psd test to account for bug fix

--- tests/test_psd.py
+++ tests/test_psd.py
@@ -8,7 +8,9 @@ def test_lambda_mu():
     # Therefore, if dispersion is fixed, slope should decrease with LWC
     # N_0 will also increases since it is directly proportional to lambda
 
-    my_model = emc2.core.model.TestConvection()
+    my_model = emc2.core.model.TestAllStratiform()
+    my_model.ds["strat_q_subcolumns_cl"] = my_model.ds[my_model.q_names_stratiform['cl']]
+    my_model.ds["strat_n_subcolumns_cl"] = my_model.ds[my_model.N_field['cl']]
     my_model = emc2.simulator.psd.calc_mu_lambda(my_model, hyd_type="cl", calc_dispersion=False)
     my_ds = my_model.ds
     assert np.all(my_ds["mu"] == 1 / 0.09)
